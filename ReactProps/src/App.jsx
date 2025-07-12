79875cd2fddbd347e2f309bccc97b0c939f215b1
Samrat Desai <samratdesai7@gmail.com>
2025-07-12 14:55:46 +0530

    some more learning in props

--- ReactProps/src/App.jsx
+++ ReactProps/src/App.jsx
@@ -2,10 +2,20 @@ import "./App.css";
 import Product from "./Product";
 
 function App() {
+  let options = ["hi-tech", "durable", "fast"];
   return (
     <>
-      <Product title="Phone" prise={50000} desc="Iphone 13 128gb storage 13px back camera flowless" />
-      <Product title="Laptop" prise={50000} desc="HP ryzen 5 8gb ram and 512gb ssd storage" />
+      <Product
+        title="Phone"
+        prise={50000}
+        desc="Iphone 13 128gb storage 13px back camera flow-less"
+        features={options}
+      />
+      <Product
+        title="Laptop"
+        prise={50000}
+        desc="HP ryzen 5 8gb ram and 512gb ssd storage"
+      />
     </>
   );
 }
--- ReactProps/src/Product.jsx
+++ ReactProps/src/Product.jsx
@@ -1,11 +1,17 @@
 import "./Product.css";
-const Product = ({ title, prise, desc = "description not given" }) => {
+const Product = ({
+  title,
+  prise,
+  desc = "description not given",
+  features =[]
+}) => {
   return (
     <>
       <div className="Product">
         <h3>{title}</h3>
         <h6>{desc}</h6>
         <h5>₹ {prise}</h5>
+        <p>{features.map((el)=><li>{el}</li>)}</p>
       </div>
     </>
   );
